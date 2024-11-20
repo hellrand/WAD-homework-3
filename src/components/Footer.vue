695c4c62e9f72838cdb46e99f5c4160d5a86706c
Scarch <stenegert10@gmail.com>
2024-11-20 17:44:28 +0200

    Made footer look better

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,34 @@
 <template>
-    <footer>
-        <p>
-            <strong>Team AV: </strong><br>
-            Sten-Egert, Gerda, Beatrice
-        </p>
-    </footer>
+    <div id="footer">
+        <footer>
+            <p>
+                <strong>Team AV: </strong><br>
+                Sten-Egert, Gerda, Beatrice
+            </p>
+        </footer>
+    </div>
 </template>
 
 <style>
+#footer {
+    position: fixed;
+    height: 120px;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    margin-bottom: 0px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
 footer {
+    width: 100%;
+    margin: 10px;
+    text-align: center;
     background-color: rgba(238, 238, 238, 0.722);
-    padding: 20px;
     border-radius: 8px;
+    padding: 10px;
     line-height: 1.5;
 }
 </style>
