9e067d7f0471239de07592e4695dc7346b8a24f9
Scarch <stenegert10@gmail.com>
2024-11-20 16:35:33 +0200

    Made a basic version of the Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer>
+        <p>
+            <strong>Team AV: </strong><br>
+            Sten-Egert, Gerda, Beatrice
+        </p>
+    </footer>
+</template>
+
+<style>
+footer {
+    background-color: rgba(238, 238, 238, 0.722);
+    padding: 20px;
+    border-radius: 8px;
+    line-height: 1.5;
+}
+</style>
