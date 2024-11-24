83b4f0b0028494aab9c11bd6afa4c372d8f0f4f0
hellrand <beatricemarj@gmail.com>
2024-11-24 18:12:53 +0200

    changed titles of the tabs and the favicon

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title || 'Default Title'; // Use default title if meta.title is not set
+    next();
+  });
+  
 createApp(App).use(store).use(router).mount('#app')
